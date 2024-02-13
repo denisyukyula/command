3164b6b28b16f0227646a570a86471ab9297baf0
Irene Khymych <himicirina1@gmail.com>
2024-02-13 11:32:52 +0200

    power of 5

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+def power_of_five(n):
+    if n == 0:
+        return False
+    while n % 5 == 0:
+        n /= 5
+    return n == 1
