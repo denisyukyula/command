54ea8ec93e9d96f684358cafd9a5ce5e964e45b8
unknown <shevchenkovlada623@gmail.com>
2024-02-13 11:42:25 +0200

    vlada

--- utils.py
+++ utils.py
@@ -10,3 +10,15 @@ def power_of_five(n):
     while n % 5 == 0:
         n /= 5
     return n == 1
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number == 2:
+        return True
+    elif number % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(number**0.5) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
